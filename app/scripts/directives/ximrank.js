43a050e0ab3cde28e6dabc936fff75a42f7c8319
Alberto <alberto.dvc@gmail.com>
2014-02-14 13:44:34 +0100

    Cleaning up.

--- app/scripts/directives/ximrank.js
+++ app/scripts/directives/ximrank.js
@@ -4,8 +4,8 @@ angular.module('xRankApp')
   .directive('ximRank', function () {
     return {
       template: '<fieldset name="create-pub" class="xim-rank"><legend>Please Vote</legend>'+
-    		'<span ng-repeat="puntuation in puntuations">'+
-    			'<input type="radio" id="rate_{{$index}}" ng-model="$parent.val" value="{{$index}}" ng-change="$parent.vote()" ng-hide="$index == 0", ng-checked="$index == 0">'+
+			'<span ng-repeat="puntuation in puntuations">'+
+    			'<input type="radio" id="rate_{{$index}}" ng-model="$parent.val" value="{{$index}}" ng-change="$parent.vote()">'+
     			'<label for="rate_{{$index}}" ng-class="{checked: $index < $parent.val}"></label>'+
     		'</span>'+
   			'<span>{{average}} ({{count}} votes)</span>'+
@@ -16,14 +16,13 @@ angular.module('xRankApp')
       restrict: 'E',
       replace: true,
       scope: {},
-      controller: ['$scope', '$http', '$window', function ($scope, $http, $window) {
-	    $scope.puntuations = [1,2,3,4,5,6];
+      controller: ['$scope', '$attrs', '$http', '$window', function ($scope, $attrs, $http, $window) {
+	    $scope.puntuations = [1,2,3,4,5];
 	    $scope.val = 0;
 	    $scope.url = $window.location.href;
 	    
 	    var refreshValorations = function(){
-	    	$http.post('api/publication', {publication:$scope.url}).success(function(data){
-	    		console.log(data);
+	    	$http.post($attrs.ximUrl+'/api/publication', {publication:$scope.url}).success(function(data){
 	    		if (data && data.publication) {
 	    			$scope.average = data.publication.average;
 	    			$scope.count = data.publication.count;
@@ -34,11 +33,11 @@ angular.module('xRankApp')
 	    refreshValorations();
 	    $scope.vote = function(){
 	    	if ($scope.url && $scope.val) {
-		    	$http.post('api/vote', {publication:$scope.url, val: $scope.val}).success(function(data){
+		    	$http.post($attrs.ximUrl+'/api/vote', {publication:$scope.url, val: $scope.val}).success(function(data){
 		    		if (data && data.notice)
 		    			refreshValorations();
 		    	});
-		   }
+		   	}
 	    };
 	  }]
     };

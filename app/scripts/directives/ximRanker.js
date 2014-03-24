5ade5ece4196ac1acfd37635ae9cb4f466118d57
Alberto <alberto.dvc@gmail.com>
2014-03-24 09:36:01 +0100

    Refactoring styles to use webfonts star icons

--- app/scripts/directives/ximRanker.js
+++ app/scripts/directives/ximRanker.js
@@ -3,10 +3,9 @@
 angular.module('xRankApp')
 .directive('ximRanker', function () {
 	return {
-		template: '<fieldset class="xim-rank"><legend>Please Vote</legend>'+
-		'<span ng-repeat="puntuation in puntuations">'+
-		'<input type="radio" id="rate_{{$index+1}}" ng-model="$parent.val" value="{{$index+1}}" ng-change="$parent.vote()">'+
-		'<label for="rate_{{$index+1}}" ng-class="{checked: $index+1 <= $parent.val}"></label>'+
+		template: '<fieldset class="xim-ranker" ng-class="{\'xim-rank-pristine\': !val}"><legend>Please Vote</legend>'+
+		'<span  ng-mouseleave="tempVal = 0">'+
+		'<span ng-repeat="puntuation in puntuations" class="xim-rank-star" ng-mouseover="$parent.tempVal = puntuation" ng-class="{\'xim-rank-star-active\': tempVal && tempVal >= puntuation, \'xim-rank-star-full\': val >= puntuation}" ng-mousedown="vote(puntuation)"></span>'+
 		'</span>'+
 		'<style>'+
 		'.xim-rank {};'+
@@ -18,9 +17,8 @@ angular.module('xRankApp')
 		controller: ['$scope', '$attrs', '$http', '$window', '$rootScope', 'urlHelper', function ($scope, $attrs, $http, $window, $rootScope, urlHelper) {
 			$scope.puntuations = [1,2,3,4,5];
 			$scope.val = 0;
+			$scope.tempVal = 0;
 			$scope.url = urlHelper.normalizeUrl($window.location.href);
-
-			$scope.uri = urlHelper;
 
 			var refreshValorations = function(){
 				$http.post(urlHelper.apiUrl()+'/api/publication', {publication:$scope.url}, {withCredentials: true}).success(function(data){
@@ -30,11 +28,12 @@ angular.module('xRankApp')
 				});
 			};
 			refreshValorations();
-			$scope.vote = function(){
-				if ($scope.url && $scope.val) {
-					$http.post(urlHelper.apiUrl()+'/api/vote', {publication:$scope.url, val: $scope.val}, {withCredentials: true}).success(function(data){
+			$scope.vote = function(val){
+				if ($scope.url && val) {
+					$http.post(urlHelper.apiUrl()+'/api/vote', {publication:$scope.url, val: val}, {withCredentials: true}).success(function(data){
 						if (data && data.notice)
 							$rootScope.$broadcast('voted', {publication:$scope.url});
+							$scope.val = val;
 					});
 				}
 			};
--- app/scripts/directives/ximrank.js
+++ app/scripts/directives/ximrank.js
@@ -3,9 +3,9 @@
 angular.module('xRankApp')
 .directive('ximRank', ['urlHelper', function (urlHelper) {
 	return {
-		template: '<span><a href="{{anchorHref}}" ng-transclude ng-if="publicationUrl"></a><span class="xim-rank">'+
-			'<span ng-repeat="puntuation in puntuations" class="xim-rank-star xim-rank-{{puntuation}}" ng-class="{checked: checked(puntuation)}"></span>'+
-			'<span ng-if="count">{{average}} ({{count}} votes)</span>'+
+		template: '<span><span ng-transclude ng-show="anchorHref"></span><span class="xim-rank">'+
+			'<span ng-repeat="puntuation in puntuations" class="xim-rank-star" ng-class="{\'xim-rank-star-full\': puntuation <= average, \'xim-rank-star-half\': puntuation > average && puntuation-1 < average}"></span>'+
+			'<span ng-if="count" class="xim-rank-average">{{average}} ({{count}} votes)</span>'+
 		'</span>'+
 		'</span>',
 		restrict: 'E, A',
@@ -15,10 +15,8 @@ angular.module('xRankApp')
 			anchorHref: '@href'
 		},
 		controller: ['$scope', '$attrs', '$http', '$window', function ($scope, $attrs, $http, $window) {
-			$scope.puntuations = [];
-			for (var i=1; i<=5; i+=0.5) {
-				$scope.puntuations.push(i);
-			}
+			$scope.puntuations = [1, 2, 3, 4, 5];
+
 			var refreshValorations = function(publication){
 				$http.post(urlHelper.apiUrl()+'/api/publication', {publication: publication}).success(function(data){
 					if (data && data.publication) {
@@ -27,33 +25,20 @@ angular.module('xRankApp')
 					}
 				});
 			};
+
 			$scope.$watch('publicationUrl', function(newVal, oldVal){
 				if (newVal)
 					refreshValorations(newVal);
 			});
+
 			if (!$attrs.href) {    
 				$scope.publicationUrl = urlHelper.normalizeUrl($window.location.href);
 			}
+
 			$scope.$on('voted', function(event, data){
 				if (data.publication === $scope.publicationUrl)
 					refreshValorations($scope.publicationUrl);
 			});
-			$scope.checked = function(puntuation) {
-				if (puntuation <= $scope.average) {
-					return true;
-				} else {
-					if ($scope.average % 1 !== 0) {
-						if (Math.floor(puntuation) === Math.floor($scope.average)){
-							return true;	
-						}
-						else {
-							return false;
-						}
-					} else {
-						return false;
-					}		
-				}	    	
-			}
 
 		}],
 		link: function postLink(scope, element, attrs) {

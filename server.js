84aedcf18165d9466dd7d76f23e80c312a9a0e1c
Alberto <alberto.dvc@gmail.com>
2014-03-25 15:17:47 +0100

    Configuring secure server.

--- server.js
+++ server.js
@@ -3,11 +3,18 @@
 var express = require('express'),
     path = require('path'),
     fs = require('fs'),
+    https = require('https'),
     mongoose = require('mongoose');
 
 /**
  * Main application file
  */
+
+//Certificates
+var privateKey  = fs.readFileSync('/etc/ssl/private/xfind.key', 'utf8');
+var certificate = fs.readFileSync('/etc/ssl/certs/xfind.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
 
 // Default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -28,7 +35,10 @@ fs.readdirSync(modelsPath).forEach(function (file) {
 if (process.env.NODE_ENV != 'production')
 	require('./lib/config/dummydata');
 
+
+
 var app = express();
+
 
 // Express settings
 require('./lib/config/express')(app);
@@ -36,10 +46,14 @@ require('./lib/config/express')(app);
 // Routing
 require('./lib/routes')(app);
 
-// Start server
-app.listen(config.port, function () {
+//Start secure server
+https.createServer(credentials, app).listen(config.port, function () {
   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
 });
+// Start server
+// app.listen(config.port, function () {
+//   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+// });
 
 // Expose app
 exports = module.exports = app;

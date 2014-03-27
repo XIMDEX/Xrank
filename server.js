f1d8df815b6b5433fd92f0dbbaef590913f7406f
Alberto <alberto.dvc@gmail.com>
2014-03-27 17:18:55 +0100

    Fix server cors config

--- server.js
+++ server.js
@@ -3,18 +3,11 @@
 var express = require('express'),
     path = require('path'),
     fs = require('fs'),
-    https = require('https'),
     mongoose = require('mongoose');
 
 /**
  * Main application file
  */
-
-//Certificates
-var privateKey  = fs.readFileSync('/etc/ssl/private/xfind.key', 'utf8');
-var certificate = fs.readFileSync('/etc/ssl/certs/xfind.pem', 'utf8');
-
-var credentials = {key: privateKey, cert: certificate};
 
 // Default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -32,13 +25,17 @@ fs.readdirSync(modelsPath).forEach(function (file) {
 });
 
 // Populate empty DB with sample data
-if (process.env.NODE_ENV != 'production')
+if (process.env.NODE_ENV == 'production') {
+	//Certificates
+	var https = require('https');
+	var privateKey  = fs.readFileSync('/etc/ssl/private/xfind.key', 'utf8');
+	var certificate = fs.readFileSync('/etc/ssl/certs/xfind.pem', 'utf8');
+	var credentials = {key: privateKey, cert: certificate};
+} else {
 	require('./lib/config/dummydata');
-
-
+}
 
 var app = express();
-
 
 // Express settings
 require('./lib/config/express')(app);
@@ -46,14 +43,16 @@ require('./lib/config/express')(app);
 // Routing
 require('./lib/routes')(app);
 
-//Start secure server
-https.createServer(credentials, app).listen(config.port, function () {
-  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-});
-// Start server
-// app.listen(config.port, function () {
-//   console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
-// });
+if (https) {
+	//Start secure server
+	https.createServer(credentials, app).listen(config.port, function () {
+	  console.log('Express secure server listening on port %d in %s mode', config.port, app.get('env'));
+	});	
+} else {
+	app.listen(config.port, function () {
+	  console.log('Express server listening on port %d in %s mode', config.port, app.get('env'));
+	});
+}
 
 // Expose app
 exports = module.exports = app;

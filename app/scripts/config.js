8fdcf905499a34351d42a52380435c27f28fe6d1
Javi Carretero <fjcarretero@ximdex.com>
2014-05-22 17:55:09 +0200

    Update config.js
    Changing test url.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,6 +1,6 @@
 angular.module('xRankApp')
     .config(function (urlHelperProvider) {
-      urlHelperProvider.setApiUrl('https://xfind.irmc.gob.es:9000');
+      urlHelperProvider.setApiUrl('https://example.org:9000');
       // Removes /xx/ idomatic path
       urlHelperProvider.setUrlTrimPattern(/(?:^http[s]?:\/\/.*?)(\/[a-z]{2}\/)/, '/xx/');
     });
